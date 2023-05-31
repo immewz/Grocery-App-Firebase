c8352aecd1757cf621eaec61089c13decef57b0d
Mewz <mghtetpai.1999@gmail.com>
2023-05-31 12:44:23 +0630

    Read Data Cloud Fire Store

--- app/src/main/java/com/mewz/grocery/data/models/GroceryModelImpl.kt
+++ app/src/main/java/com/mewz/grocery/data/models/GroceryModelImpl.kt
@@ -1,12 +1,14 @@
 package com.mewz.grocery.data.models
 
 import com.mewz.grocery.data.vos.GroceryVO
+import com.mewz.grocery.network.CloudFirestoreFirebaseApiImpl
 import com.mewz.grocery.network.FirebaseApi
 import com.mewz.grocery.network.RealtimeDatabaseFirebaseApiImpl
 
 object GroceryModelImpl: GroceryModel {
 
-    override var mFirebaseApi: FirebaseApi = RealtimeDatabaseFirebaseApiImpl
+//    override var mFirebaseApi: FirebaseApi = RealtimeDatabaseFirebaseApiImpl
+    override var mFirebaseApi: FirebaseApi = CloudFirestoreFirebaseApiImpl
 
     override fun getGroceries(onSuccess: (List<GroceryVO>) -> Unit, onFailure: (String) -> Unit) {
         mFirebaseApi.getGroceries(onSuccess, onFailure)
---
+++ app/src/main/java/com/mewz/grocery/network/CloudFirestoreFirebaseApiImpl.kt
@@ -0,0 +1,41 @@
+package com.mewz.grocery.network
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.mewz.grocery.data.vos.GroceryVO
+
+object CloudFirestoreFirebaseApiImpl: FirebaseApi {
+
+    val db = Firebase.firestore
+
+    override fun getGroceries(
+        onSuccess: (groceries: List<GroceryVO>) -> Unit,
+        onFailure: (String) -> Unit
+    ) {
+        db.collection("groceries")
+            .get()
+            .addOnSuccessListener { result ->
+                val groceriesList: MutableList<GroceryVO> = arrayListOf()
+                for (document in result){
+                    val data = document.data
+                    var grocery = GroceryVO()
+                    grocery.name = data["name"] as String
+                    grocery.description = data["description"] as String
+                    grocery.amount = (data["amount"] as Long).toInt()
+                    groceriesList.add(grocery)
+                }
+                onSuccess(groceriesList)
+            }
+            .addOnFailureListener { exception ->
+                onFailure(exception.message ?: "Please check connection")
+            }
+    }
+
+    override fun addGrocery(name: String, description: String, amount: Int) {
+
+    }
+
+    override fun deleteGrocery(name: String) {
+
+    }
+}
