ebb8102ea21d1abded85bbc6c7d2f31f329118bd
Mewz <mghtetpai.1999@gmail.com>
2023-05-31 11:16:23 +0630

    Read Data Complete

--- app/src/main/java/com/mewz/grocery/activities/MainActivity.kt
+++ app/src/main/java/com/mewz/grocery/activities/MainActivity.kt
@@ -1,12 +1,6 @@
 package com.mewz.grocery.activities
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
--- app/src/main/java/com/mewz/grocery/mvp/views/MainView.kt
+++ app/src/main/java/com/mewz/grocery/mvp/views/MainView.kt
@@ -3,6 +3,5 @@ package com.mewz.grocery.mvp.views
 import com.mewz.grocery.data.vos.GroceryVO
 
 interface MainView: BaseView {
-
     fun showGroceryData(groceryList: List<GroceryVO>)
 }
